8348eb9c203d3bb5e27e414e7c694fc90011afae
Ahmed Sherif <amedhussein7075@gmail.com>
2023-04-02 23:42:23 +0200

    booking statuses

--- resources/views/dashboard/layout/sidebar.blade.php
+++ resources/views/dashboard/layout/sidebar.blade.php
@@ -142,6 +142,30 @@
                     </li>
                 </ul>
             </li>
+            <li class="menu">
+                <a href="#booking" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">
+                    <div class="">
+                        <div class="icon-container">
+                            <i data-feather="archive"></i><span class="icon-name"> الحجوزات </span>
+                        </div>
+                    </div>
+                    <div>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
+                             stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+                             class="feather feather-chevron-right">
+                            <polyline points="9 18 15 12 9 6"></polyline>
+                        </svg>
+                    </div>
+                </a>
+                <ul class="collapse submenu list-unstyled" id="booking" data-parent="#accordionExample">
+                    <li>
+                        <a href="#"> الحجوزات </a>
+                    </li>
+                    <li>
+                        <a href="{{route('dashboard.booking_statuses.index')}}"> حالات الحجز </a>
+                    </li>
+                </ul>
+            </li>
 
 
 
--- routes/dashboard/dashboard.router.php
+++ routes/dashboard/dashboard.router.php
@@ -27,6 +27,7 @@ Route::group(['middleware' => ['auth:dashboard']], static function () {
     Route::view('/', 'dashboard.home')->name('home');
     require __DIR__ . '/core.routes.php';
     require __DIR__ . '/order.routes.php';
+    require __DIR__ . '/booking.routes.php';
     require __DIR__ . '/setting.routes.php';
 });
 
