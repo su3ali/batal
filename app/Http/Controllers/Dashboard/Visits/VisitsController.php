e06d9cbb1cd1c7f34c6fc95e311de87a0a1994a4
azmykamel <azmy.abuzeid@gmail.com>
2023-05-15 23:39:01 +0300

    visit

--- app/Http/Controllers/Dashboard/Visits/VisitsController.php
+++ app/Http/Controllers/Dashboard/Visits/VisitsController.php
@@ -46,6 +46,8 @@ class VisitsController extends Controller
                 ->addColumn('control', function ($row) {
 
                     $html = '
+                    <a href="#" class="mr-2 btn btn-outline-primary btn-sm">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-navigation"><polygon points="3 11 22 2 13 21 11 13 3 11"></polygon></svg>
 
                                 <a href="' . route('dashboard.visits.show', $row->id) . '" class="mr-2 btn btn-outline-primary btn-sm">
                             <i class="far fa-eye fa-2x"></i>
