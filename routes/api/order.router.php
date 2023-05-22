138b1229c335754e29f8e8a7b9796cbd10c56fcf
Ahmed Sherif <amedhussein7075@gmail.com>
2023-05-22 22:59:36 +0300

    orders

--- routes/api/order.router.php
+++ routes/api/order.router.php
@@ -4,11 +4,19 @@
 use App\Http\Controllers\Api\Bookings\BookingsController;
 use App\Http\Controllers\Api\Coupons\CouponsController;
 use App\Http\Controllers\Api\Orders\OrdersController;
+use App\Http\Controllers\Api\Statuses\StatusController;
 
 Route::prefix('orders')->middleware('auth')->group(function () {
 
     Route::get('/', [OrdersController::class, 'myOrders']);
     Route::get('/{id}', [OrdersController::class, 'orderDetails']);
+//    Route::post('refund')
+
+});
+Route::prefix('statuses')->middleware('auth')->group(function () {
+
+    Route::get('/bookings', [StatusController::class, 'bookingsStatuses']);
+    Route::get('/orders', [StatusController::class, 'ordersStatuses']);
 //    Route::post('refund')
 
 });
