517c84b75381f9ead41d1ab528eebc2193ca7c4c
azmykamel <azmy.abuzeid@gmail.com>
2023-05-25 01:27:08 +0300

    notify

--- resources/views/dashboard/layout/sidebar.blade.php
+++ resources/views/dashboard/layout/sidebar.blade.php
@@ -318,8 +318,7 @@
             <li class="menu">
                 <a href="#notification" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">
                     <div class="">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-star"><polygon points="12 2 15.09 8.26 22 9.27 17 14.14 18.18 21.02 12 17.77 5.82 21.02 7 14.14 2 9.27 8.91 8.26 12 2"></polygon></svg>
-
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-bell"><path d="M18 8A6 6 0 0 0 6 8c0 7-3 9-3 9h18s-3-2-3-9"></path><path d="M13.73 21a2 2 0 0 1-3.46 0"></path></svg>
                         <span>إدارة الاشعارات</span>
                     </div>
                     <div>
