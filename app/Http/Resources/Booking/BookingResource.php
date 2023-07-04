fae4b9272a91cf7d64e95e45633b4af14b9b400e
azmykamel <azmy.abuzeid@gmail.com>
2023-07-05 02:34:20 +0300

    test

--- app/Http/Resources/Booking/BookingResource.php
+++ app/Http/Resources/Booking/BookingResource.php
@@ -20,7 +20,7 @@ class BookingResource extends JsonResource
             'booking_no' => $this->booking_no,
             'status' => $this->visit? StatusResource::make($this->visit->status) : null,
             'category' => CategoryBasicResource::make($this->category),
-            'services' => $services->isNotEmpty() ? ServiceResource::collection($services) : null,
+            'services' => $services->isNotEmpty() ? ServiceResource::collection($services) : [],
             'image' => $this->category->slug? asset($this->category->slug) : '',
             'date' => Carbon::parse($this->date)->format('d M'),
             'time_start' => Carbon::parse($this->time)->format('g:i A'),
