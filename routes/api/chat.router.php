ae4fea821e5fb63515bf6f505bb30d69dc98a987
Ahmed Sherif <amedhussein7075@gmail.com>
2023-07-12 01:11:54 +0300

    pusher

--- routes/api/chat.router.php
+++ routes/api/chat.router.php
@@ -10,7 +10,8 @@ Route::group(['prefix' => 'chat'], function () {
             $socketId = $request->input('socket_id');
             $channelName = $request->input('channel_name');
             $channelData = ['user_id' => $user->id, 'name' => $user->name];
-            $authData = Broadcast::auth($request);
+            $authDatas = $pusher->socket_auth($request->get('channel_name'), $request->get('socket_id'));
+            $authData['auth'] = json_decode($authDatas)->auth;
             $authData['channel_data'] = json_encode($channelData);
 
             return $authData;
