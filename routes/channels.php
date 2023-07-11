1a2fad8303463abcd6041f3e9a5430cb5aef5eca
Ahmed Sherif <amedhussein7075@gmail.com>
2023-07-11 22:39:08 +0300

    pusher

--- routes/channels.php
+++ routes/channels.php
@@ -22,7 +22,7 @@ Broadcast::channel('chat_message.{chat}', function ($user, $roomId) {
 Broadcast::channel('chat_room.{room_id}', function ($user, $room) {
 //    return  $room->admin_id == $admin->id;
     return class_basename(get_class($user)) == 'Admin' ||
-    $user->id == $room->sender->id
+    $user->id == \App\Models\Room::query()->find($room)->sender->id
         ? ['id' => $user->id, 'name' => $user->first_name, 'type' => class_basename(get_class($user)), 'room' => $room]
         : false;
 });
