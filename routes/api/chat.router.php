1a1a47e95a0ee8076e117abafd4f664bccc6efc8
Ahmed Sherif <amedhussein7075@gmail.com>
2023-07-11 21:22:55 +0300

    11

--- routes/api/chat.router.php
+++ routes/api/chat.router.php
@@ -4,7 +4,7 @@ use App\Http\Controllers\Api\Chat\UserChatController;
 use Illuminate\Support\Facades\Broadcast;
 
 Route::group(['prefix' => 'chat'], function () {
-    Broadcast::routes(['middleware' => ['auth:sanctum','abilities:user']]);
+    Broadcast::routes(['middleware' => ['auth:sanctum']]);
 
     Route::get('my_rooms', [UserChatController::class, 'myRooms']);
     Route::post('broadcast', [UserChatController::class, 'saveMessage']);
--- routes/channels.php
+++ routes/channels.php
@@ -14,12 +14,15 @@ use Illuminate\Support\Facades\Broadcast;
 */
 Broadcast::channel('chat_message.{chat}', function ($user, $roomId) {
 //    return  $room->admin_id == $admin->id;
-    if ($user){
-        return ['id' => $user->id, 'name' => $user->first_name, 'type' => class_basename(get_class($user))];
-    }else
-        return false;
+    return class_basename(get_class($user)) == 'Admin' ||
+    $user->id == \App\Models\Room::query()->find($roomId)->sender->id
+        ? ['user_id' => $user->id, 'user_type' => class_basename(get_class($user)), 'room_id' => $roomId]
+        : false;
 });
 Broadcast::channel('chat_room.{room_id}', function ($user, $room) {
 //    return  $room->admin_id == $admin->id;
-    return ['id' => $user->id, 'name' => $user->first_name, 'type' => class_basename(get_class($user)), 'room' => $room];
+    return class_basename(get_class($user)) == 'Admin' ||
+    $user->id == $room->sender->id
+        ? ['id' => $user->id, 'name' => $user->first_name, 'type' => class_basename(get_class($user)), 'room' => $room]
+        : false;
 });
