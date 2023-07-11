1f64e1849b23eac5b3acbef63f12c79c6d4df4b5
Ahmed Sherif <amedhussein7075@gmail.com>
2023-07-11 21:52:18 +0300

    pusher

--- routes/api/chat.router.php
+++ routes/api/chat.router.php
@@ -4,8 +4,6 @@ use App\Http\Controllers\Api\Chat\UserChatController;
 use Illuminate\Support\Facades\Broadcast;
 
 Route::group(['prefix' => 'chat'], function () {
-    Broadcast::routes(['prefix'=>'api', 'middleware' => ['auth:sanctum', 'abilities:user']]);
-
     Route::get('my_rooms', [UserChatController::class, 'myRooms']);
     Route::post('broadcast', [UserChatController::class, 'saveMessage']);
     Route::get('loadChat', [UserChatController::class, 'loadChat']);
