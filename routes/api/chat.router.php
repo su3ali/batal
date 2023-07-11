0d300a48c3cee93a85a8aa0e0a11f279ca3dcc84
Ahmed Sherif <amedhussein7075@gmail.com>
2023-07-12 01:09:02 +0300

    pusher

--- routes/api/chat.router.php
+++ routes/api/chat.router.php
@@ -5,13 +5,12 @@ use App\Http\Controllers\Api\Chat\UserChatController;
 Route::group(['prefix' => 'chat'], function () {
     Route::post('/broadcasting/auth', function (\Illuminate\Http\Request $request){
         if (Auth::check()) {
-            $pusher = new \Pusher\Pusher(env('PUSHER_APP_KEY'), env('PUSHER_APP_SECRET'), env('PUSHER_APP_ID'));
+            $pusher = new \Pusher\Pusher('87ed15aef6ced76b1507', 'd8f21b90df201e227ad8', "1629640");
             $user = Auth::user();
             $socketId = $request->input('socket_id');
             $channelName = $request->input('channel_name');
             $channelData = ['user_id' => $user->id, 'name' => $user->name];
-            $authDatas = $pusher->socket_auth($request->get('channel_name'), $request->get('socket_id'));
-            $authData['auth'] = json_decode($authDatas)->auth;
+            $authData = Broadcast::auth($request);
             $authData['channel_data'] = json_encode($channelData);
 
             return $authData;
