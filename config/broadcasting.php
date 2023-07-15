a6f6904d9e447e057f62f4b46d740273f69b0a00
azmykamel <azmy.abuzeid@gmail.com>
2023-07-15 18:05:28 +0300

    test

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -37,8 +37,10 @@ return [
             'app_id' => env('PUSHER_APP_ID'),
             'options' => [
                 'cluster' => env('PUSHER_APP_CLUSTER'),
-                'encrypted' => true,
+                'encrypted' => false,
                 'useTLS' => true,
+                'host' => 'https://cp.batal.com.sa/',
+                'port' => 443,
                 'curl_options' => [
                     CURLOPT_SSL_VERIFYHOST => 0,
                     CURLOPT_SSL_VERIFYPEER => 0,
