daea82dee4a204b8a61c51e26ab61e37bf702251
azmykamel <azmy.abuzeid@gmail.com>
2023-07-15 17:35:57 +0300

    test

--- routes/api/chat.router.php
+++ routes/api/chat.router.php
@@ -4,14 +4,13 @@ use App\Http\Controllers\Api\Chat\UserChatController;
 
 Route::group(['prefix' => 'chat'], function () {
     Route::post('/broadcasting/auth', function (\Illuminate\Http\Request $request){
-
         if (Auth::check()) {
             $pusher = new \Pusher\Pusher('87ed15aef6ced76b1507', 'd8f21b90df201e227ad8', "1629640");
             $user = Auth::user();
             $socketId = $request->input('socket_id');
             $channelName = $request->input('channel_name');
             $channelData = ['user_id' => $user->id, 'name' => $user->name];
-            $authDatas = $pusher->socketAuth($request->get('channel_name'), $request->get('socket_id'), ['user_id' => $user->id]);
+            $authDatas = $pusher->socket_auth($request->get('channel_name'), $request->get('socket_id'), json_encode($channelData));
             $authData['auth'] = json_decode($authDatas)->auth;
             $authData['channel_data'] = json_encode($channelData);
 
