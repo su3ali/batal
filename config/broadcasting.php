6fa6da675fcbd431c4c49d65413c618e01c0682d
azmykamel <azmy.abuzeid@gmail.com>
2023-07-15 18:03:10 +0300

    test

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -38,6 +38,7 @@ return [
             'options' => [
                 'cluster' => env('PUSHER_APP_CLUSTER'),
                 'encrypted' => true,
+                'useTLS' => true,
                 'curl_options' => [
                     CURLOPT_SSL_VERIFYHOST => 0,
                     CURLOPT_SSL_VERIFYPEER => 0,
