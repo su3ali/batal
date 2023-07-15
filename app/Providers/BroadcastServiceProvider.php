888d2994fc55fbdb18c2a17880d2b0de823e1de6
azmykamel <azmy.abuzeid@gmail.com>
2023-07-15 20:29:11 +0300

    test

--- app/Providers/BroadcastServiceProvider.php
+++ app/Providers/BroadcastServiceProvider.php
@@ -14,7 +14,7 @@ class BroadcastServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        Broadcast::routes(['middleware' => ['auth:dashboard']]);
+        Broadcast::routes();
         Broadcast::routes(['prefix' => 'api/chat']);
         require base_path('routes/channels.php');
     }
