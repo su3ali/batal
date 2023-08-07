b4e1dfd9103c7aad2ef0438ac8503915b2ea84f2
azmykamel <azmy.abuzeid@gmail.com>
2023-08-08 01:17:03 +0300

    test

--- app/Providers/BroadcastServiceProvider.php
+++ app/Providers/BroadcastServiceProvider.php
@@ -14,7 +14,7 @@ class BroadcastServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        Broadcast::routes(['middleware'=>'web']);
+        Broadcast::routes(['middleware'=>'auth,web']);
 //        Broadcast::routes(['middleware'=>'auth:sanctum','prefix' => 'api/chat']);
         require base_path('routes/channels.php');
     }
