4f377f477a172c7dafbc6a0ac5114aa23d7d254a
azmykamel <azmy.abuzeid@gmail.com>
2023-09-07 01:47:55 +0300

    test

--- routes/web.php
+++ routes/web.php
@@ -9,6 +9,7 @@ Route::get('/clear', function() {
     Artisan::call('view:clear');
     Artisan::call('route:clear');
     Artisan::call('permission:cache-reset');
+    Artisan::call('optimize:clear');
 
     return "Cleared!";
 
