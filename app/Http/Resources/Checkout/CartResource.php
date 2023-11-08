9306562ee4f1003495cde8a901bc65fc95e21c0e
vicSahmtech <m.ahmad@sahmtech.com>
2023-11-08 21:54:15 +0300

    ..

--- app/Http/Resources/Checkout/CartResource.php
+++ app/Http/Resources/Checkout/CartResource.php
@@ -4,6 +4,8 @@ namespace App\Http\Resources\Checkout;
 
 use App\Http\Resources\service\AdditionsResource;
 use App\Models\Addition;
+use App\Models\ContractPackage;
+use App\Models\ContractPackagesUser;
 use Illuminate\Http\Resources\Json\JsonResource;
 
 class CartResource extends JsonResource
@@ -17,24 +19,40 @@ class CartResource extends JsonResource
     public function toArray($request)
     {
         $images = [];
-        foreach ($this->service->serviceImages as $serviceImage) {
-            if ($serviceImage->image) {
-                $images[] = asset($serviceImage->image);
+        if ($this->service) {
+            foreach ($this->service->serviceImages as $serviceImage) {
+                if ($serviceImage->image) {
+                    $images[] = asset($serviceImage->image);
+                }
             }
         }
+
+
+        $contractPackagesUser =  ContractPackagesUser::where('user_id', auth()->user()->id)->where('contract_packages_id', $this->contract_package_id)->first();
+
+        $tempTotal = ($this->price * $this->quantity);
+        if ($contractPackagesUser) {
+            
+            $contractPackage = ContractPackage::where('id', $contractPackagesUser->contract_packages_id)->first();
+            if ($this->quantity <  ($contractPackage->visit_number - $contractPackagesUser->used)) {
+                $tempTotal = 0;
+            } else {
+                $tempTotal = ($this->quantity - ($contractPackage->visit_number - $contractPackagesUser->used)) * $this->price;
+            }
+        }
+
         return [
             'id' => $this->id,
-            'service_id' => $this->service_id,
-            'category_id' => $this->service->category->id,
-            'category_title' => $this->service->category->title,
-            'service_title' => $this->service?->title,
+            'service_id' => $this->service_id??0,
+            'category_id' => $this->service?->category->id??0,
+            'category_title' => $this->service?->category->title??'',
+            'service_title' => $this->service?->title??'',
             'quantity' => $this->quantity,
-            'file' => $this->files ? asset($this->files) : '',
             'service_image' => $images,
-            'price' => $this->contract_package_id ? $this->price : ($this->price * $this->quantity),
+            'price' => $tempTotal,
             'package_id' => $this->contract_package_id,
             'type' => $this->type,
-            'package_name' => $this->type == 'package'? $this->package->name : null
+            'package_name' => $this->type == 'package' ? $this->package->name : null
         ];
     }
 }
