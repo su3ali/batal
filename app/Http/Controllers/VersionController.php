1be08e8c9da0aa8a863ce76575ef941f90666493
vicSahmtech <m.ahmad@sahmtech.com>
2023-11-08 22:03:10 +0300

    ..

--- app/Http/Controllers/VersionController.php
+++ app/Http/Controllers/VersionController.php
@@ -10,6 +10,27 @@ class VersionController extends Controller
 {
     use ApiResponse;
 
+
+    protected function compareVersions($version1, $version2)
+    {
+        $version1Parts = explode('.', $version1);
+        $version2Parts = explode('.', $version2);
+
+
+        for ($i = 0; $i < 3; $i++) {
+            $part1 = isset($version1Parts[$i]) ? (int)$version1Parts[$i] : 0;
+            $part2 = isset($version2Parts[$i]) ? (int)$version2Parts[$i] : 0;
+
+            if ($part1 < $part2) {
+                return -1; // Version 1 is less than Version 2
+            } elseif ($part1 > $part2) {
+                return 1;  // Version 1 is greater than Version 2
+            }
+        }
+
+        return 0; // Versions are equal
+    }
+
     protected function checkUpdate(Request $request)
     {
         $rules = [
@@ -18,7 +39,10 @@ class VersionController extends Controller
         ];
         $request->validate($rules, $request->all());
         $version = Version::where('os', $request->os)->first();
-        if ($version->version == $request->version) {
+        if ($this->compareVersions($request->version, $version->version) >= 0) {
+
+
+
             $this->body['allowed'] = 1;
             return self::apiResponse(200, __('api.allowed'), $this->body);
         } else {
