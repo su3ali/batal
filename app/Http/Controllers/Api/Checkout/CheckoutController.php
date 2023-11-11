e1676d7af0154704b70672de7efa720e568eb25d
vicSahmtech <m.ahmad@sahmtech.com>
2023-11-11 20:47:21 +0300

    ..

--- app/Http/Controllers/Api/Checkout/CheckoutController.php
+++ app/Http/Controllers/Api/Checkout/CheckoutController.php
@@ -39,6 +39,34 @@ class CheckoutController extends Controller
         $this->middleware('localization');
     }
 
+    protected function checkTimeDate(Request $request)
+    {
+        $user = auth()->user('sanctum');
+        $carts = Cart::query()->where('user_id', $user->id)->get();
+        $regionId = UserAddresses::where('id', \request()->query('user_address_id'))->first()->region_id;
+        foreach ($carts as $cart) {
+            $groupIds = CategoryGroup::where('category_id', $cart->category_id)->pluck('group_id')->toArray();
+            $countGroup = Group::where('active', 1)->whereHas('regions', function ($qu) use ($regionId) {
+                $qu->where('region_id',  $regionId);
+            })->whereIn('id', $groupIds)->count();
+            $countInBooking = Booking::whereHas('visit', function ($q) {
+                $q->whereNotIn('visits_status_id', [5, 6]);
+            })->whereHas(
+                'address.region',
+                function ($q) use ($regionId) {
+
+                    $q->where('id',  $regionId);
+                }
+            )->where([['category_id', '=', $cart->category_id], ['date', '=',  $cart->date], ['time', '=', $cart->time]])
+                ->count();
+
+            if (($countInBooking ==  $countGroup)) {
+                return self::apiResponse(400, __('api.There is a category for which there are currently no technical groups available'), $this->body);
+            }
+        }
+        return self::apiResponse(200, __('api.order created successfully'), $this->body);
+    }
+
     protected function checkout(Request $request)
     {
         $rules = [
@@ -112,7 +140,7 @@ class CheckoutController extends Controller
                 }
             }
             $total = $this->calc_total($carts);
-            return $this->saveOrder($user, $request, $total, $carts, $uploadImage, $uploadFile);
+            return $this->saveOrder($user, $request, $total, $carts, $uploadImage, $uploadFile, $parent_payment_method);
         }
     }
 
@@ -125,6 +153,7 @@ class CheckoutController extends Controller
         }
         return array_sum($total);
     }
+
 
     private function saveOrder($user, $request, $total, $carts, $uploadImage, $uploadFile, $parent_payment_method)
     {
@@ -209,7 +238,13 @@ class CheckoutController extends Controller
                 if (($visit->get()->count()) < ($group->get()->count())) {
                     $assign_to_id = $group->whereNotIn('id', $visit->pluck('assign_to_id')->toArray())->inRandomOrder()->first()->id;
                 } else {
-                    $assign_to_id = $visit->inRandomOrder()->first()->assign_to_id;
+                    $alreadyTaken = Visit::where('start_time', $cart->time)->whereIn('booking_id', $booking_id)->whereIn('assign_to_id', $activeGroups)->get();
+                    if ($alreadyTaken->isNotEmpty()) {
+                        $ids = $alreadyTaken->pluck('assign_to_id')->toArray();
+                        $assign_to_id = $visit->whereNotIn('assign_to_id', $ids)->inRandomOrder()->first()->assign_to_id;
+                    } else {
+                        $assign_to_id = $visit->inRandomOrder()->first()->assign_to_id;
+                    }
                 }
             }
 
@@ -422,7 +457,7 @@ class CheckoutController extends Controller
 
         $this->wallet($user, $total);
         Cart::query()->whereIn('id', $carts->pluck('id'))->delete();
-      //  $this->body['order_id'] = $order->id;
+        //  $this->body['order_id'] = $order->id;
         return self::apiResponse(200, __('api.order created successfully'), $this->body);
     }
 
