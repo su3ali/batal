81cd4ec369169a6035004f36f1f26ab16aaf2f4d
Victor-Ahmad <victor.ahmad.mail@gmail.com>
2024-05-02 14:39:24 +0300

    .

--- app/Http/Controllers/Dashboard/NotificationController.php
+++ app/Http/Controllers/Dashboard/NotificationController.php
@@ -16,88 +16,178 @@ use Illuminate\Support\Facades\Notification;
 class NotificationController extends Controller
 {
     use NotificationTrait;
+    // public function showNotification()
+    // {
+
+    //     $technicians = Technician::query()->whereNotNull('fcm_token')->pluck('name','id')->all();
+    //     $customers = User::query()->whereNotNull('fcm_token')->get();
+
+
+    //     return view('dashboard.notification.notification',compact('customers','technicians'));
+    // }
     public function showNotification()
     {
+        $type = request()->input('type');
 
-        $technicians = Technician::query()->whereNotNull('fcm_token')->pluck('name','id')->all();
-        $customers = User::query()->whereNotNull('fcm_token')->get();
+        $subjects = User::whereNotNull('fcm_token');
+
+        if (request()->ajax()) {
+            if ($type == 'technician') {
+                $subjects = Technician::whereNotNull('fcm_token');
+            } elseif ($type == 'customersWithNoOrders') {
+                $subjects = User::whereNotNull('fcm_token')->doesntHave('orders');
+            }
+
+            $subjects = $subjects->get();
+
+            return response()->json(['subjects' => $subjects]);
+        }
 
 
-        return view('dashboard.notification.notification',compact('customers','technicians'));
+
+        $subjects = $subjects->get();
+        return view('dashboard.notification.notification', compact('subjects'));
     }
 
     public function sendNotification(Request $request)
     {
 
         $request->validate([
-            'customer_id'=>'nullable',
-            'technician_id'=>'nullable',
-            'title'=>'required',
-            'message'=>'required',
-            'type'=>'required'
+            'subject_id' => 'nullable',
+            'technician_id' => 'nullable',
+            'title' => 'required',
+            'message' => 'required',
+            'type' => 'required'
         ]);
-        if ($request->type == 'customer'){
-            if ($request->customer_id == 'all'){
+        if ($request->type == 'customer') {
+            if ($request->subject_id == 'all') {
                 $FcmTokenArray = User::whereNotNull('fcm_token')->get()->pluck('fcm_token');
-            }else{
-                $user = User::where('id',$request->customer_id)->first('fcm_token');
+            } else {
+                $user = User::where('id', $request->subject_id)->first('fcm_token');
                 $FcmToken = $user->fcm_token;
             }
 
             $type = 'customer';
-        }else{
-            if ($request->technician_id == 'all'){
+        } else if ($request->type == 'customersWithNoOrders') {
+            if ($request->subject_id == 'all') {
+                $FcmTokenArray = User::whereNotNull('fcm_token')->doesntHave('orders')->get()->pluck('fcm_token');
+            } else {
+                $FcmToken = User::where('id', $request->subject_id)->first('fcm_token');
+                // $FcmToken = $user->fcm_token;
+            }
+            $type = 'customer';
+        } else {
+            if ($request->technician_id == 'all') {
                 $allTechn = Technician::whereNotNull('fcm_token')->get();
 
                 $FcmTokenArray = $allTechn->pluck('fcm_token');
 
 
-                foreach ($allTechn as $tech){
+                foreach ($allTechn as $tech) {
                     Notification::send(
                         $tech,
-                        new SendPushNotification($request->title,$request->message)
+                        new SendPushNotification($request->title, $request->message)
                     );
                 }
-
-            }else{
-                $technician = Technician::where('id',$request->technician_id)->first();
+            } else {
+                $technician = Technician::where('id', $request->technician_id)->first();
                 $FcmToken = $technician->fcm_token;
 
                 Notification::send(
                     $technician,
-                    new SendPushNotification($request->title,$request->message)
+                    new SendPushNotification($request->title, $request->message)
                 );
-
             }
 
             $type = 'technician';
-
         }
 
 
-        if (isset($FcmToken) && $FcmToken == null){
+        if (isset($FcmToken) && $FcmToken == null) {
 
             return redirect()->back()->withErrors(['fcm_token' => 'لا يمكن ارسال الاشعارت لعدم توفر رمز الجهاز']);
-
-        }elseif(isset($FcmTokenArray) && count($FcmTokenArray) == 0){
+        } elseif (isset($FcmTokenArray) && count($FcmTokenArray) == 0) {
             return redirect()->back()->withErrors(['fcm_token' => 'لا يمكن ارسال الاشعارت لعدم توفر رمز الجهاز']);
         }
 
+        $message = str_replace('&nbsp;', ' ', strip_tags($request->message));
 
         $notification = [
-            'device_token' => isset($FcmToken) ?[$FcmToken] : $FcmTokenArray,
+            'device_token' => isset($FcmToken) ? [$FcmToken] : $FcmTokenArray,
             'title' => $request->title,
-            'message' => strip_tags($request->message),
-            'type'=>$type??'',
-            'code'=> 2
+            'message' =>  $message,
+            'type' => $type ?? '',
+            'code' => 2
         ];
 
-      $this->pushNotification($notification);
+        $this->pushNotification($notification);
         session()->flash('success');
         return redirect()->back();
-
     }
+    // public function sendNotification(Request $request)
+    // {
+
+    //     $request->validate([
+    //         'customer_id' => 'nullable',
+    //         'technician_id' => 'nullable',
+    //         'title' => 'required',
+    //         'message' => 'required',
+    //         'type' => 'required'
+    //     ]);
+    //     if ($request->type == 'customer') {
+    //         if ($request->customer_id == 'all') {
+    //             $FcmTokenArray = User::whereNotNull('fcm_token')->get()->pluck('fcm_token');
+    //         } else {
+    //             $user = User::where('id', $request->customer_id)->first('fcm_token');
+    //             $FcmToken = $user->fcm_token;
+    //         }
+
+    //         $type = 'customer';
+    //     } else {
+    //         if ($request->technician_id == 'all') {
+    //             $allTechn = Technician::whereNotNull('fcm_token')->get();
+
+    //             $FcmTokenArray = $allTechn->pluck('fcm_token');
 
 
+    //             foreach ($allTechn as $tech) {
+    //                 Notification::send(
+    //                     $tech,
+    //                     new SendPushNotification($request->title, $request->message)
+    //                 );
+    //             }
+    //         } else {
+    //             $technician = Technician::where('id', $request->technician_id)->first();
+    //             $FcmToken = $technician->fcm_token;
 
+    //             Notification::send(
+    //                 $technician,
+    //                 new SendPushNotification($request->title, $request->message)
+    //             );
+    //         }
+
+    //         $type = 'technician';
+    //     }
+
+
+    //     if (isset($FcmToken) && $FcmToken == null) {
+
+    //         return redirect()->back()->withErrors(['fcm_token' => 'لا يمكن ارسال الاشعارت لعدم توفر رمز الجهاز']);
+    //     } elseif (isset($FcmTokenArray) && count($FcmTokenArray) == 0) {
+    //         return redirect()->back()->withErrors(['fcm_token' => 'لا يمكن ارسال الاشعارت لعدم توفر رمز الجهاز']);
+    //     }
+
+
+    //     $notification = [
+    //         'device_token' => isset($FcmToken) ? [$FcmToken] : $FcmTokenArray,
+    //         'title' => $request->title,
+    //         'message' => strip_tags($request->message),
+    //         'type' => $type ?? '',
+    //         'code' => 2
+    //     ];
+
+    //     $this->pushNotification($notification);
+    //     session()->flash('success');
+    //     return redirect()->back();
+    // }
 }
--- resources/views/dashboard/notification/notification.blade.php
+++ resources/views/dashboard/notification/notification.blade.php
@@ -1,4 +1,3 @@
-
 @extends('dashboard.layout.layout')
 
 @section('sub-header')
@@ -7,8 +6,8 @@
 
             <a href="javascript:void(0);" class="sidebarCollapse" data-placement="bottom">
                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
-                     stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
-                     class="feather feather-menu">
+                    stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+                    class="feather feather-menu">
                     <line x1="3" y1="12" x2="21" y2="12"></line>
                     <line x1="3" y1="6" x2="21" y2="6"></line>
                     <line x1="3" y1="18" x2="21" y2="18"></line>
@@ -22,8 +21,8 @@
                         <nav class="breadcrumb-one" aria-label="breadcrumb">
                             <ol class="breadcrumb mb-0 py-2">
                                 <li class="breadcrumb-item">
-                                    <a
-                                                                        href="{{route('dashboard.home')}}">{{__('dash.home')}}</a></li>
+                                    <a href="{{ route('dashboard.home') }}">{{ __('dash.home') }}</a>
+                                </li>
                                 <li class="breadcrumb-item active" aria-current="page">الاشعارات</li>
                             </ol>
                         </nav>
@@ -36,7 +35,7 @@
         </header>
     </div>
 
-    {{--    @include('dashboard.order_statuses.create')--}}
+    {{--    @include('dashboard.order_statuses.create') --}}
 @endsection
 
 @section('content')
@@ -50,63 +49,79 @@
                         <h3>ارسال اشعارات</h3>
                     </div>
                     <div class="col-md-12">
-                        <form action="{{route('dashboard.notification.sendNotification')}}" method="post" class="form-horizontal"
-
-                              enctype="multipart/form-data" id="create_order_status_form" data-parsley-validate="">
+                        <form action="{{ route('dashboard.notification.sendNotification') }}" method="post"
+                            class="form-horizontal" enctype="multipart/form-data" id="create_order_status_form"
+                            data-parsley-validate="">
                             @csrf
                             <div class="box-body">
 
-                                <div class="form-row mb-2">
+                                <div class="form-row mb-3">
 
 
-                                    <div class="form-group col-md-6">
+                                    <div class="form-group col-md-4">
                                         <div class="n-chk">
                                             <label class="new-control new-radio radio-classic-primary">
-                                                <input type="radio" class="new-control-input type" value="customer" checked name="type">
+                                                <input type="radio" class="new-control-input type" value="customer"
+                                                    checked name="type">
                                                 <span class="new-control-indicator"></span>العملاء
                                             </label>
                                         </div>
                                         @error('type')
-                                        <div class="alert alert-danger">{{ $message }}</div>
+                                            <div class="alert alert-danger">{{ $message }}</div>
                                         @enderror
 
                                     </div>
-                                    <div class="form-group col-md-6">
+                                    <div class="form-group col-md-4">
+                                        <div class="n-chk">
+                                            <label class="new-control new-radio radio-classic-primary">
+                                                <input type="radio" class="new-control-input type"
+                                                    value="customersWithNoOrders" name="type">
+                                                <span class="new-control-indicator"></span>العملاء الجدد
+                                            </label>
+                                        </div>
+                                        @error('type')
+                                            <div class="alert alert-danger">{{ $message }}</div>
+                                        @enderror
+
+                                    </div>
+                                    <div class="form-group col-md-4">
 
                                         <div class="n-chk">
                                             <label class="new-control new-radio radio-classic-primary">
-                                                <input type="radio" class="new-control-input type" value="technician" name="type">
+                                                <input type="radio" class="new-control-input type" value="technician"
+                                                    name="type">
                                                 <span class="new-control-indicator"></span>الفنيين
                                             </label>
                                         </div>
                                         @error('type')
-                                        <div class="alert alert-danger">{{ $message }}</div>
+                                            <div class="alert alert-danger">{{ $message }}</div>
                                         @enderror
 
                                     </div>
 
 
-                                    <div class="form-group col-md-6 div-customer">
-                                        <label for="customer">العملاء</label>
-                                        <select  class="form-control customer_id"  style="width: 100%; padding: 8px"
-                                                name="customer_id">
+                                    <div class="form-group col-md-4">
+                                        <label class="lable_subject_id" for="subject_id">العملاء</label>
+                                        <select class="form-control subject_id" style="width: 100%; padding: 8px"
+                                            name="subject_id">
                                             <option value="all" selected>الكل</option>
-                                            @foreach($customers as $customer)
-                                            <option value="{{$customer->id}}">{{$customer->first_name}} - {{ $customer->phone}}</option>
-                                                @endforeach
+                                            @foreach ($subjects as $subject)
+                                                <option value="{{ $subject->id }}">{{ $subject->first_name }} -
+                                                    {{ $subject->phone }}</option>
+                                            @endforeach
                                         </select>
-                                        @error('customer_id')
-                                        <div class="alert alert-danger">{{ $message }}</div>
+                                        @error('subject_id')
+                                            <div class="alert alert-danger">{{ $message }}</div>
                                         @enderror
 
                                     </div>
 
-                                    <div class="form-group col-md-6 div-technician" style="display:none;">
+                                    {{-- <div class="form-group col-md-6 div-technician" style="display:none;">
                                         <label for="customer">الفنيين</label>
                                         <select  class="form-control technician_id" disabled style="width: 100%; padding: 8px"
                                                 name="technician_id">
                                             <option value="all" selected>الكل</option>
-                                            @foreach($technicians as $key => $technician)
+                                            @foreach ($technicians as $key => $technician)
                                                 <option value="{{$key}}">{{$technician}}</option>
                                             @endforeach
                                         </select>
@@ -114,27 +129,23 @@
                                         <div class="alert alert-danger">{{ $message }}</div>
                                         @enderror
 
-                                    </div>
+                                    </div> --}}
 
                                     <div class="form-group col-md-6">
                                         <label for="title">العنوان</label>
-                                        <input type="text" name="title" class="form-control"
-                                               id="title"
-                                               placeholder="أدخل العنوان"
-                                               required
-                                        >
+                                        <input type="text" name="title" class="form-control" id="title"
+                                            placeholder="أدخل العنوان" required>
                                         @error('title')
-                                        <div class="alert alert-danger">{{ $message }}</div>
+                                            <div class="alert alert-danger">{{ $message }}</div>
                                         @enderror
                                     </div>
 
                                     <div class="form-group col-md-12">
 
                                         <label for="message">الرساله</label>
-                                        <textarea name="message" id="message" class="ckeditor" cols="30"
-                                                  rows="10"></textarea>
+                                        <textarea name="message" id="message" class="ckeditor" cols="30" rows="10"></textarea>
                                         @error('message')
-                                        <div class="alert alert-danger">{{ $message }}</div>
+                                            <div class="alert alert-danger">{{ $message }}</div>
                                         @enderror
 
                                     </div>
@@ -150,9 +161,9 @@
                                     </div>
 
                                     <div class="col-md-6 text-right">
-                                        <button type="submit" class="btn btn-primary">{{__('dash.save')}}</button>
-                                        <button class="btn" data-dismiss="modal"><i
-                                                class="flaticon-cancel-12"></i> {{__('dash.close')}}
+                                        <button type="submit" class="btn btn-primary">{{ __('dash.save') }}</button>
+                                        <button class="btn" data-dismiss="modal"><i class="flaticon-cancel-12"></i>
+                                            {{ __('dash.close') }}
                                         </button>
                                     </div>
                                 </div>
@@ -169,8 +180,51 @@
 @endsection
 
 @push('script')
-
     <script type="text/javascript">
+        $("body").on('change', '.type, .gender_filter', function() {
+            var label = $('.lable_subject_id');
+            var selectedType = $('.type:checked').val();
+
+
+            $.ajax({
+                url: '{{ route('dashboard.notification.showNotification') }}',
+                type: 'GET',
+                data: {
+                    type: selectedType,
+
+                },
+                success: function(response) {
+                    var subjects = response.subjects;
+                    var selectElement = $('.subject_id');
+                    selectElement.empty();
+                    selectElement.append('<option value="all" selected>الكل</option>');
+                    subjects.forEach(function(subject) {
+                        if (selectedType == 'technician') {
+                            selectElement.append('<option value="' + subject.id + '">' + subject
+                                .user_name + ' - ' + subject.phone + '</option>');
+                        } else {
+                            selectElement.append('<option value="' + subject.id + '">' + subject
+                                .first_name + ' - ' + subject.phone + '</option>');
+                        }
+
+                    });
+
+                    // Update the label based on the selected type
+                    if (selectedType == 'technician') {
+                        label.text('الفنيين');
+                    } else if (selectedType == 'customersWithNoOrders') {
+                        label.text('العملاء الجدد');
+                    } else {
+                        label.text('العملاء');
+                    }
+                },
+                error: function(error) {
+                    console.log(error);
+                }
+            });
+        });
+    </script>
+    {{-- <script type="text/javascript">
         $("body").on('change', '.type', function () {
 
             if ($(this).val() == 'technician') {
@@ -180,17 +234,20 @@
                 $('.div-technician').show();
 
 
+            } else if$(this).val() (=='customersWithNoOrders'){
+                $('.div-technician').hide();
+                $('.customer_id').attr('disabled',false);
+                $('.technician_id').attr('disabled',true);
+                $('.div-customer').show();
             } else {
                 $('.div-technician').hide();
                 $('.customer_id').attr('disabled',false);
                 $('.technician_id').attr('disabled',true);
                 $('.div-customer').show();
-
             }
 
         })
 
 
-    </script>
-
+    </script> --}}
 @endpush
