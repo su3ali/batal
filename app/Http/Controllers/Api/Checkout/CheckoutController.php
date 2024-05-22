e24a8e9d085e861667a48f18e8073f15caa073c6
Victor-Ahmad <victor.ahmad.mail@gmail.com>
2024-05-22 07:42:48 +0300

    .

--- app/Http/Controllers/Api/Checkout/CheckoutController.php
+++ app/Http/Controllers/Api/Checkout/CheckoutController.php
@@ -238,7 +238,7 @@ class CheckoutController extends Controller
                 if (($visit->get()->count()) < ($group->get()->count())) {
                     $assign_to_id = $group->whereNotIn('id', $visit->pluck('assign_to_id')->toArray())->inRandomOrder()->first()->id;
                 } else {
-                    $alreadyTaken = Visit::where('start_time', $cart->time)->whereIn('booking_id', $booking_id)->whereIn('assign_to_id', $activeGroups)->get();
+                    $alreadyTaken = Visit::where('end_time', $cart->time)->whereIn('booking_id', $booking_id)->whereIn('assign_to_id', $activeGroups)->get();
                     if ($alreadyTaken->isNotEmpty()) {
                         $ids = $alreadyTaken->pluck('assign_to_id')->toArray();
                         $assign_to_id = $visit->whereNotIn('assign_to_id', $ids)->inRandomOrder()->first()->assign_to_id;
