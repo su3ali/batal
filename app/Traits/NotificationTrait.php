f25f76043d50298611bb3d7bdaad38d7c436a0da
BadrAkbik <badrakbik1@email.com>
2024-06-22 17:41:20 +0300

    notification

--- app/Traits/NotificationTrait.php
+++ app/Traits/NotificationTrait.php
@@ -31,7 +31,7 @@ trait NotificationTrait
 //          'data' => $data
 //      ];
 
-        $fields = [
+/*         $fields = [
             'registration_ids' => $device_token,
 //          'notification' => $notification,
             "content_available" => true,
@@ -45,7 +45,51 @@ trait NotificationTrait
             'sound' => 'default',
             "priority" => "HIGH",
             "mutable-content" => 1,
-        ];
+        ]; */
+
+        $fields = [];
+        if ($notification['type'] == 'customer') {
+            $fields = [
+                'registration_ids' => $device_token,
+                'notification' => [
+                    'title' => $notification['title'],
+                    'body' => $notification['message'],
+                ],
+                "content_available" => true,
+                "android" => [
+                    "priority" => "HIGH"
+                ],
+                "ios" => [
+                    "priority" => "HIGH",
+                    // "apns" => [
+                    //     "payload" => [
+                    //         "aps" => [
+                    //             "contentAvailable" => true
+                    //         ]
+                    //     ]
+                    // ]
+                ],
+                'data' => $data,
+                'sound' => 'default',
+                "priority" => "HIGH",
+                "mutable-content" => true,
+            ];
+        } else {
+            $fields = [
+                'registration_ids' => $device_token,
+                "content_available" => true,
+                "android" => [
+                    "priority" => "HIGH"
+                ],
+                "ios" => [
+                    "priority" => "HIGH",
+                ],
+                'data' => $data,
+                'sound' => 'default',
+                "priority" => "HIGH",
+                "mutable-content" => true,
+            ];
+        }
 
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_URL, 'https://fcm.googleapis.com/fcm/send');
@@ -75,7 +119,7 @@ trait NotificationTrait
             'data' => $notification['data'],
         ];
 
-        $fields = [
+        /* $fields = [
             'registration_ids' => $device_token,
             "content_available" => true,
             "android" => [
@@ -86,7 +130,76 @@ trait NotificationTrait
             ],
             'data' => $data,
             "priority" => "HIGH",
-        ];
+        ]; */
+
+        $fields = [];
+        if ($notification['fromFunc'] == 'latlong') {
+            $fields = [
+                'registration_ids' => $device_token,
+                "content_available" => true,
+                "android" => [
+                    "priority" => "HIGH"
+                ],
+                "ios" => [
+                    "priority" => "HIGH",
+                    // "apns" => [
+                    //     "payload" => [
+                    //         "aps" => [
+                    //             "contentAvailable" => true
+                    //         ]
+                    //     ]
+                    // ]
+                ],
+                'data' => $data,
+                "priority" => "HIGH",
+                "mutable-content" => true,
+            ];
+        } else {
+            $statusList = [
+                'طلبك باقي نرسلك افضل الأخصائيين عندنا',
+                ' فريقنا جايين لعندك ياريت تجهز مكان العمل',
+                ' فريق اهتمام بدأ بالخدمة هالحين',
+                'ارسلنا لك طلب تسليم اذا شغلنا زين وافق عليها',
+                'حبينا نشكرك على انتظارك طلبك مكتمل الحين',
+                'تم إلغاء الطلب للأسباب التالية: ',
+                ' للزيارة رقم ',
+            ];
+            $title = $notification['data']['order_details']['group']['name'] . $statusList[6] . $notification['data']['order_details']['id'];
+            $body = '';
+            $notification = json_decode(json_encode($notification));
+
+            $booking_details = $notification->data->order_details->booking_details;
+            if (!(is_null($booking_details->status))) {
+
+                if ($booking_details->status->id == 6) {
+                    $body = $statusList[$booking_details->status->id - 1] . $notification->data->order_details->cancel_reason->reason;
+                } else {
+                    $body = $statusList[$booking_details->status->id - 1];
+                }
+            } else {
+
+                $body = $statusList[0];
+            }
+
+
+            $fields = [
+                'registration_ids' => $device_token,
+                "content_available" => true,
+                'notification' => [
+                    'title' => $title,
+                    'body' => $body,
+                ],
+                "android" => [
+                    "priority" => "HIGH"
+                ],
+                "ios" => [
+                    "priority" => "HIGH",
+                ],
+                'data' => $data,
+                "priority" => "HIGH",
+                "mutable-content" => true,
+            ];
+        }
 
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_URL, 'https://fcm.googleapis.com/fcm/send');
