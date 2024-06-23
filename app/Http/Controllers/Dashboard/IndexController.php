120a8bbf0482e92c31441df8532f3f3a3742f6ca
BadrAkbik <badrakbik1@email.com>
2024-06-23 12:24:42 +0300

    fixing showing cancled order today

--- app/Http/Controllers/Dashboard/IndexController.php
+++ app/Http/Controllers/Dashboard/IndexController.php
@@ -121,7 +121,7 @@ class IndexController extends Controller
         ));
         $sells_chart_2->dataset(__('dash.orders'), 'line', $all_sell_values2);
         $canceled_orders = Order::where('status_id', 5)->where('is_active', 1)->count();
-        $canceled_orders_today = Order::where('status_id', 5)->where('is_active', 1)->where('updated_at', $now)->count();
+        $canceled_orders_today = Order::where('status_id', 5)->where('is_active', 1)->whereDate('updated_at', $today)->count();
         return view('dashboard.home', compact('canceled_orders', 'canceled_orders_today', 'tech_visits_today', 'client_orders_today', 'sells_chart_1', 'sells_chart_2', 'customers', 'client_orders', 'technicians', 'tech_visits'));
     }
     private function __chartOptions($title)
