eccb3b5c9ef6b08abd3453a597bdce3db3fcd08b
BadrAkbik <badrakbik1@email.com>
2024-06-29 18:56:16 +0300

    orders show repair

--- resources/views/dashboard/orders/index.blade.php
+++ resources/views/dashboard/orders/index.blade.php
@@ -40,7 +40,7 @@
 @section('content')
     <div class="layout-px-spacing">
 
-        <div class="row layout-top-spacing">
+        <div class="row layout-top-spacing" style="width: 95%">
 
             <div class="col-xl-12 col-lg-12 col-sm-12  layout-spacing">
                 <div class="widget-content widget-content-area br-6">
