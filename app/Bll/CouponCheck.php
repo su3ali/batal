de77f50317372704164c427e8d9bbe4fe1a4f7f5
BadrAkbik <badrakbik1@email.com>
2024-06-29 18:31:27 +0300

    .

--- app/Bll/CouponCheck.php
+++ app/Bll/CouponCheck.php
@@ -20,37 +20,6 @@ class CouponCheck
             $response = ['error' => __("api.Invalid Code !")];
             return $response;
         }
-/* 
-        if(!isset($coupon->service_id) && !isset($coupon->category_id)){
-            $discount = $coupon->type == 'percentage'?($coupon->value/100)*$total : $coupon->value;
-        }elseif (isset($coupon->service_id) && !isset($coupon->category_id)){
-            $used = false;
-            $discount = 0;
-            foreach($carts as $cart){
-                if($cart->service_id === $coupon->service_id){
-                    $used = true;
-                    $cart_total = ($cart->price) * $cart->quantity;
-                    $discount += $coupon->type == 'percentage'?($coupon->value/100)*$cart_total : $coupon->value;
-                }
-            }
-            if(!$used){
-                return ['error' => __('api.This coupon con not be used to any of these services !')];
-            }
-        }else{
-            $used = false;
-            $discount = 0;
-            foreach($carts as $cart){
-                if($cart->category_id === $coupon->category_id){
-                    $used = true;
-                    $cart_total = ($cart->price) * $cart->quantity;
-                    $discount += $coupon->type == 'percentage'?($coupon->value/100)*$cart_total : $coupon->value;
-                }
-            }
-            if(!$used){
-                return ['error' => __('api.This coupon con not be used to any of these services !')];
-            }
-        } */
-
         if ($coupon->min_price && $coupon->min_price > $total){
             $response = ['error' => __("api.Your order price didn't reach the minimum value for the coupon !")];
             return $response;
@@ -70,9 +39,4 @@ class CouponCheck
         $response = ['success' => __('api.coupon applied !')];
         return $response;
     }
-
-/*     public function check_coupon_services_match()
-    {
-    
-    } */
 }
