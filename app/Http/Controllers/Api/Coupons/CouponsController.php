11eb2d477a8216fab67a900c3b23ab38ae0af956
BadrAkbik <badrakbik1@email.com>
2024-06-29 19:55:30 +0300

    code enhancement

--- app/Http/Controllers/Api/Coupons/CouponsController.php
+++ app/Http/Controllers/Api/Coupons/CouponsController.php
@@ -41,43 +41,16 @@ class CouponsController extends Controller
         }
         $coupon = Coupon::query()->where('code', $code)->first();
         
-        
         if ($coupon){
             $total = $this->calc_total($carts);
-
-            if(!isset($coupon->service_id) && !isset($coupon->category_id)){
-                $discount = $coupon->type == 'percentage'?($coupon->value/100)*$total : $coupon->value;
-            }elseif (isset($coupon->service_id) && !isset($coupon->category_id)){
-                $used = false;
-                $discount = 0;
-                foreach($carts as $cart){
-                    if($cart->service_id === $coupon->service_id){
-                        $used = true;
-                        $cart_total = ($cart->price) * $cart->quantity;
-                        $discount += $coupon->type == 'percentage'?($coupon->value/100)*$cart_total : $coupon->value;
-                    }
-                }
-                if(!$used){
-                    return self::apiResponse(400, __('api.This coupon con not be used to any of these services !'), $this->body);
-                }
-            }else{
-                $used = false;
-                $discount = 0;
-                foreach($carts as $cart){
-                    if($cart->category_id === $coupon->category_id){
-                        $used = true;
-                        $cart_total = ($cart->price) * $cart->quantity;
-                        $discount += $coupon->type == 'percentage'?($coupon->value/100)*$cart_total : $coupon->value;
-                    }
-                }
-                if(!$used){
-                    return self::apiResponse(400, __('api.This coupon con not be used to any of these services !'), $this->body);
-                }
-            }
             $coupon_user = CouponUser::query()->where('coupon_id', $coupon->id)
                 ->where('user_id', auth()->user()->id)->get();
             $check = new CouponCheck();
-
+            $match_response = $check->check_coupon_services_match($coupon, $total, $carts);
+            if($match_response['error']){
+                return self::apiResponse(400, __('api.This coupon con not be used to any of these services !'), $this->body);
+            }
+            $discount = $match_response['discount'];
             $res = $check->check_avail($coupon, $coupon_user, $total);
             if (key_exists('success', $res)){
                 CouponUser::query()->create([
