fe481a4102ca9ccbea4d190888330d9566eba2ff
vagrant <vagrant@homestead>
2024-07-24 08:19:08 +0000

    Revert "Upgrade Laravel version from 8.x to 9.x"
    This reverts commit d8863d3a73f85fa3c1bd53e6d6ac51bbccc3f5fa.

--- database/migrations/2023_05_15_005338_add_cols_to_orders_table.php
+++ database/migrations/2023_05_15_005338_add_cols_to_orders_table.php
@@ -6,11 +6,16 @@ use Illuminate\Support\Facades\Schema;
 
 class AddColsToOrdersTable extends Migration
 {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
     public function up()
     {
         Schema::table('orders', function (Blueprint $table) {
             $table->unsignedBigInteger('service_id')->nullable()->change();
-            $table->float('price')->nullable();
+            $table->float('price')->nullable()->change();
             $table->bigInteger('quantity')->default(0)->after('price')->nullable()->change();
             $table->float('discount')->after('user_id')->nullable();
             $table->double('sub_total')->after('discount')->nullable();
@@ -19,6 +24,11 @@ class AddColsToOrdersTable extends Migration
         });
     }
 
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
     public function down()
     {
         Schema::table('orders', function (Blueprint $table) {
